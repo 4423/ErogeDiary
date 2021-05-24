1729a77e70fdaec9f3f4d098f08e878eeca5b2a3
4423 <hanamiya4423@gmail.com>
2021-05-25 00:01:07 +0900

    Add converter from play time span to string

---
+++ ErogeDaily/Views/Converters/PlayTimeToStringConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ErogeDaily.Views.Converters
+{
+    public class PlayTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not TimeSpan)
+            {
+                return null;
+            }
+
+            var ts = (TimeSpan)value;
+
+            if (parameter != null && ts.TotalSeconds == 0)
+            {
+                return parameter;
+            }
+
+            var res = "";
+            if (((int)ts.TotalHours) > 0)
+            {
+                res += $"{(int)ts.TotalHours}時間";
+            }
+            if (ts.Minutes > 0)
+            {
+                res += $"{ts.Minutes}分";
+            }
+            if (ts.Seconds > 0)
+            {
+                res += $"{ts.Seconds}秒";
+            }
+            return res;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
