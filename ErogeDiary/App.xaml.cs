cc81ba63338b916783b3b36435c711436fee5155
4423 <hanamiya4423@gmail.com>
2023-01-04 14:07:32 +0900

    Format App.xaml.cs

--- ErogeDiary/App.xaml.cs
+++ ErogeDiary/App.xaml.cs
@@ -2,7 +2,6 @@
 using ErogeDiary.Models;
 using ErogeDiary.Models.Database;
 using ErogeDiary.Models.ErogameScape;
-using ErogeDiary.ViewModels;
 using ErogeDiary.ViewModels.Dialogs;
 using ErogeDiary.ViewModels.Pages;
 using ErogeDiary.Views;
@@ -13,61 +12,54 @@ using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Mvvm;
 using Prism.Unity;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using Unity;
 
-namespace ErogeDiary
+namespace ErogeDiary;
+
+public partial class App : PrismApplication
 {
-    public partial class App : PrismApplication
+    protected override void OnStartup(StartupEventArgs e)
     {
-        protected override void OnStartup(StartupEventArgs e)
+        base.OnStartup(e);
+
+        using (var dbContext = new ErogeDiaryDbContext())
         {
-            base.OnStartup(e);
-
-            using (var dbContext = new ErogeDiaryDbContext())
-            {
-                dbContext.Database.Migrate();
-            }
+            dbContext.Database.Migrate();
         }
+    }
 
-        protected override Window CreateShell()
-        {
-            return Container.Resolve<MainWindow>();
-        }
+    protected override void ConfigureViewModelLocator()
+    {
+        base.ConfigureViewModelLocator();
+        ViewModelLocationProvider.Register<HomePage, HomeViewModel>();
+        ViewModelLocationProvider.Register<GameDetailPage, GameDetailViewModel>();
+    }
 
-        protected override void ConfigureViewModelLocator()
-        {
-            base.ConfigureViewModelLocator();
-            ViewModelLocationProvider.Register<HomePage, HomeViewModel>();
-            ViewModelLocationProvider.Register<GameDetailPage, GameDetailViewModel>();
-        }
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.Register<ErogeDiaryDbContext>();
+        containerRegistry.Register<GameMonitor>();
+        containerRegistry.Register<IErogameScapeAccess, WebScrapingErogameScapeAccess>();
 
-        protected override void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-            containerRegistry.Register<ErogeDiaryDbContext>();
-            containerRegistry.Register<GameMonitor>();
-            containerRegistry.Register<IErogameScapeAccess, WebScrapingErogameScapeAccess>();
+        containerRegistry.Register<IMessageDialog, MessageDialog>();
+        containerRegistry.Register<IOpenFileDialog, OpenFileDialog>();
 
-            containerRegistry.Register<IMessageDialog, MessageDialog>();
-            containerRegistry.Register<IOpenFileDialog, OpenFileDialog>();
+        containerRegistry.RegisterDialog<GameEditDialog, GameEditDialogViewModel>();
+        containerRegistry.RegisterDialog<GameRegistrationDialog, GameRegistrationDialogViewModel>();
+        containerRegistry.RegisterDialog<RootRegistrationDialog, RootRegistrationDialogViewModel>();
+        containerRegistry.RegisterDialog<RootEditDialog, RootEditDialogViewModel>();
+        containerRegistry.RegisterDialog<RootRemoveDialog, RootRemoveDialogViewModel>();
+    }
 
-            containerRegistry.RegisterDialog<GameEditDialog, GameEditDialogViewModel>();
-            containerRegistry.RegisterDialog<GameRegistrationDialog, GameRegistrationDialogViewModel>();
-            containerRegistry.RegisterDialog<RootRegistrationDialog, RootRegistrationDialogViewModel>();
-            containerRegistry.RegisterDialog<RootEditDialog, RootEditDialogViewModel>();
-            containerRegistry.RegisterDialog<RootRemoveDialog, RootRemoveDialogViewModel>();
-        }
+    protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+    {
+        base.ConfigureModuleCatalog(moduleCatalog);
+        moduleCatalog.AddModule(typeof(NavigationModule));
+    }
 
-        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
-        {
-            base.ConfigureModuleCatalog(moduleCatalog);
-            moduleCatalog.AddModule(typeof(NavigationModule));
-        }
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
     }
 }
