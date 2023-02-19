1b104030701953dcf318d2ef34d873af68c90654
4423 <hanamiya4423@gmail.com>
2023-02-19 21:49:52 +0900

    Polish accent colors

--- ErogeDiary/Models/AccentColors.cs
+++ ErogeDiary/Models/AccentColors.cs
@@ -7,42 +7,34 @@ public class AccentColors: List<AccentColor>
 {
     public AccentColors()
     {
-        // https://valleysoftware.com.au/development/windows-accent-colours/
-        Add("#FFB900"); //Yellow Gold
-        Add("#FF8C00"); //Gold
-        Add("#F7630C"); //Orange Bright
-        Add("#CA5010"); //Orange Dark
-        Add("#DA3B01"); //Rust
-        Add("#EF6950"); //Pale Rust
-        Add("#D13438"); //Brick Red
-        Add("#FF4343"); //Mod Red
-
-        Add("#E74856"); //Pale Red
-        Add("#E81123"); //Red
-        Add("#EA005E"); //Rose Bright
-        Add("#C30052"); //Rose
-        Add("#E3008C"); //Plum Light
-        Add("#BF0077"); //Plum
-        Add("#C239B3"); //Orchid Light
-        Add("#9A0089"); //Orchid
-
-        Add("#0078D7"); //Default Blue
-        Add("#0063B1"); //Navy Blue
-        Add("#8E8CD8"); //Purple Shadow
-        Add("#6B69D6"); //Purple Shadow Dark
-        Add("#8764B8"); //Iris Pastel
-        Add("#744DA9"); //Iris Spring
-        Add("#B146C2"); //Violet Red Light
-        Add("#881798"); //Violet Red
-
-        Add("#0099BC"); //Cool Blue Bright
-        Add("#2D7D9A"); //Cool Blue
-        Add("#00B7C3"); //Seafoam
-        Add("#038387"); //Seafoam Team
-        Add("#00B294"); //Mint Light
-        Add("#018574"); //Mint Dark
-        Add("#00CC6A"); //Turf Green
-        Add("#10893E"); //Sport Green
+        Add("#d46262"); //Light Red
+        Add("#d48062"); //Light Orange
+        Add("#d4a862"); //Light Yellow
+        Add("#b5bf71"); //Light Uguisu
+        Add("#88b56c"); //Light YellowGreen
+        Add("#63a678"); //Light Green
+        Add("#63a69e"); //Light Asagi
+        Add("#6389a6"); //Light Skyblue
+        Add("#636da6"); //Light Blue
+        Add("#8463a6"); //Light Purple
+        Add("#a6639b"); //Light Pink
+        Add("#a66378"); //Light Azuki
+        Add("#ba2d2d"); //Dark Red
+        Add("#c75734"); //Dark Orange
+        Add("#cf9436"); //Dark Yellow
+        Add("#a2b038"); //Dark Uguisu
+        Add("#62a638"); //Dark YellowGreen
+        Add("#339652"); //Dark Green
+        Add("#33968a"); //Dark Asagi
+        Add("#336b96"); //Dark Skyblue
+        Add("#334296"); //Dark Blue
+        Add("#643396"); //Dark Purple
+        Add("#963386"); //Dark Pink
+        Add("#963352"); //Dark Azuki
+        Add("#949494"); //Gray 1
+        Add("#808080"); //Gray 2
+        Add("#696969"); //Gray 3
+        Add("#545454"); //Gray 4
     }
 
     private void Add(string colorCode)
