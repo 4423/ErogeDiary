5723ccd692b7dc74dec893490c9058b8e88e0289
4423 <hanamiya4423@gmail.com>
2023-02-23 02:04:50 +0900

    Fix mouse wheel behavior on histogram

---
+++ ErogeDiary/Views/Behaviors/PassThroughMouseWheelBehavior.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ErogeDiary.Views.Behaviors;
+
+public class PassThroughMouseWheelBehavior : Behavior<UIElement>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.PreviewMouseWheel += PreviewMouseWheel;
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.PreviewMouseWheel -= PreviewMouseWheel;
+        base.OnDetaching();
+    }
+
+    private void PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        if (!e.Handled)
+        {
+            e.Handled = true;
+            var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+            {
+                RoutedEvent = UIElement.MouseWheelEvent,
+                Source = sender
+            };
+            var parent = ((Control)sender).Parent as UIElement;
+            parent?.RaiseEvent(eventArg);
+        }
+    }
+}
